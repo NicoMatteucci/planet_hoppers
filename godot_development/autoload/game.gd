e36ea7ba357a5f92041f3b31cc7658ed7f1e8f0f
NicoMatteucci <matteuccinicolas02@gmail.com>
2025-08-20 04:08:49 -0300

    interacciones tactiles agregdas

--- godot_development/autoload/game.gd
+++ godot_development/autoload/game.gd
@@ -7,3 +7,10 @@ func _ready():
 	var plyr := preload("res://scenes/player.tscn").instantiate()
 	plyr.position = get_viewport_rect().size / 2
 	add_child(plyr)
+	
+	var controles_scene := preload("res://scenes/controls.tscn")
+	var controles := controles_scene.instantiate()
+	add_child(controles)
+
+func _on_TouchScreenButton_pressed():
+	print("¡Botón presionado!")
